32d2168794023be481b233aea8af5faf81e2d158
Brian Gardner <briangardner@users.noreply.github.com>
2019-02-09 12:52:00 -0500

    Adding Stock Item

---
+++ src/GildedRoseCore.Console/Decorators/StockItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ConsoleApplication;
+
+namespace GildedRoseCore.Console.Decorators
+{
+    public abstract class StockItem
+    {
+        private readonly Item _item;
+
+        protected StockItem(Item item)
+        {
+            _item = item;
+        }
+
+        public virtual void UpdateItem()
+        {
+            Age();
+            Deteriorate();
+        }
+        protected abstract void Age();
+        protected abstract void Deteriorate();
+    }
+}
