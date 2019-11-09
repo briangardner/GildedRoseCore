f9956108b2335e6cf3a00e25bc791707dffc7340
brian_gardner <1761566+briangardner@users.noreply.github.com>
2019-11-09 12:28:08 -0500

    WIP

--- src/GildedRoseCore.Console.Tests/TestAssemblyTests.cs
+++ src/GildedRoseCore.Console.Tests/TestAssemblyTests.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using ConsoleApplication;
 using Xunit;
 
 namespace Tests
 {
     public class TestAssemblyTests
     {
+        private readonly Program _program;
+        public TestAssemblyTests()
+        {
+            _program = new Program();
+        }
         [Fact]
         public void TestTheTruth() 
         {
